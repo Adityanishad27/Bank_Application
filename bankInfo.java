dac6ec91502ac6f4ac4e955f68c62af666870b35
Aditya Nishad <97361495+Adityanishad27@users.noreply.github.com>
2023-12-25 11:09:16 +0530

    Add files via upload

---
+++ bankInfo.java
@@ -0,0 +1,38 @@
+public class bankInfo {
+    private String accno;
+    private String name;
+    private String acc_type;
+    private long balance;
+
+    public String getAccno() {
+        return accno;
+    }
+
+    public void setAccno(String accno) {
+        this.accno = accno;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getAcc_type() {
+        return acc_type;
+    }
+
+    public void setAcc_type(String acc_type) {
+        this.acc_type = acc_type;
+    }
+
+    public long getBalance() {
+        return balance;
+    }
+
+    public void setBalance(long balance) {
+        this.balance = balance;
+    }
+}
